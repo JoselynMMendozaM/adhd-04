d1edc3b8ef0d4973c129cc6eff6cf5767490db1d
JoselynMMendozaM <159175891+JoselynMMendozaM@users.noreply.github.com>
2024-06-11 09:25:41 -0600

    Delete ts/ejemplo.js

--- ts/ejemplo.js
+++
@@ -1,9 +0,0 @@
-function sumar() {
-    var _a;
-    var num1 = parseFloat(document.getElementById('textNum1').value);
-    var num2 = document.getElementById("txtNum2");
-    var n2 = parseFloat(num2.value);
-    var suma = num1 + n2;
-    var suma2 = num1 + parseFloat(num2.value);
-    (_a = document.getElementById('result')) === null || _a === void 0 ? void 0 : _a.innerHTML = "la suma es: " + suma;
-}
